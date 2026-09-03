fb243cf8fc0054fc5f1ebab1849909bc28525b30
Vivek Tiwari <145338708+vivek8620@users.noreply.github.com>
2026-09-03 19:05:51 +0530

    refactor: comment out stats component inclusion in homepage

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 
 <!-- Impact & Stats Section (Moved below Hero) -->
-<?php include("components/homeComp/stats.php") ?>
+<?php //include("components/homeComp/stats.php") ?>
 
 
 <!-- Transformation / Beyond Development Section -->
